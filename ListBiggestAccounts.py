892e5deeb4231da7360162c6337f9657c62244f0
qualialibre <31902221+qualialibre@users.noreply.github.com>
2018-05-31 19:07:03 -0400

    Using getaccount instead of getwalletacounts

--- ListBiggestAccounts.py
+++ ListBiggestAccounts.py
@@ -2,23 +2,44 @@ import os
 import sys
 import requests,json
 
-#ListBiggestAccounts 10000
-minBalance = int(sys.argv[1])
+#NOTE: normal wallet default port is 4003, Testnet is 4103
+if len(sys.argv) != 3:
+	print("usage : ListBiggestAccounts.py LocalWalletPort MinimumBalance\nNOTE: normal wallet default port is 4003, Testnet is 4103")
+	exit()
 
-url = "http://127.0.0.1:4103"
-payload = '{"jsonrpc":"2.0","method":"getwalletaccounts","params":{"max":99999},"id":123}'
-headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.post(url, data=payload, headers=headers)
-jres = json.loads(r.text)
-#print(str(jres))
-accounts =  jres["result"]
-bigAccounts = []
-for account in accounts:
-    sourceAccount = int(account['account'])
-    srcBalance = account["balance"]
-    if (srcBalance > minBalance):
-        bigAccounts.append((srcBalance, sourceAccount))
+port = int(sys.argv[1])
+minBalance = int(sys.argv[2])
 
-bigAccounts.sort()
-for a in bigAccounts:
-    print("Account {0: <7} balance {1: <5}".format(a[1], a[0]))
+f = open("AccountList.txt", "w")
+
+url = "http://127.0.0.1:{}".format(port)
+print("connecting to {}".format(url))
+accounts = []
+try:
+    #for a in range(0, 999):
+    for a in range(0, 2000000):
+        if (a%500) == 0:
+            print("scanning account {} to {}".format(a, a+500))
+        payload = '{"jsonrpc":"2.0","method":"getaccount","params":{"account":' + str(a) + '},"id":123}'
+        headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+        r = requests.post(url, data=payload, headers=headers)
+        jres = json.loads(r.text)
+        #print(str(jres))
+        acnt =  jres["result"]
+        sourceAccount = int(acnt['account'])
+        srcBalance = acnt["balance"]
+        if (srcBalance > minBalance):
+            name = acnt["name"]
+            accounts.append((srcBalance, sourceAccount, name))
+except:
+       pass
+       
+print("found {} accounts".format(len(accounts)))
+accounts.sort()
+for a in accounts:
+    msg = "Account {0: <7} balance {1: <20} name '{2: <1}'".format(a[1], a[0], a[2])
+    f.write(msg + "\n")
+    print(msg)
+   
+f.close()
+print("results saved in AccountList.txt")
